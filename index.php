41468f1477f62a7b5ca1354960a7da5138f2d55d
mbeutel <mbeutel@cip58.informatik.uni-osnabrueck.de>
2016-07-28 11:03:27 +0200

    deploy testing

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php include_once("./build/home.html"); ?>
 <?php error_reporting(E_ALL);?>
 <link href='/build/css/style.css' type='text/css' rel='stylesheet'/>
+<script src="js/three.min.js"></script>
+<script src="js/lib/fileLoader.js"></script>
+<script src="js/lib/controls/firstPerson.js"></script>
+<script src="js/lib/controls/interaction.js"></script>
+<script src="js/lib/controls/Player.js"></script>
+<script src="js/lib/controls/PointerLockControls.js"></script>
+<script src="js/lib/audio/audioLoader.js"></script>
+<script src="js/lib/fire/VolumetricFire.js"></script>
+<script src="js/lib/smoke/smoke.js"></script>
+<script src="js/lib/fire/firePlacement.js"></script>
+<script src="js/lib/stats/stats.min.js"></script>
+<script src="js/lib/xml_loader/objects.js"></script>
+<script src="js/game.js"></script>
