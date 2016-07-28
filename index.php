9cbd7f95c833c1149d9997a894463a5fb9fd3546
mbeutel <mbeutel@cip58.informatik.uni-osnabrueck.de>
2016-07-28 11:13:23 +0200

    test

--- index.php
+++ index.php
@@ -2,16 +2,18 @@
 <?php error_reporting(E_ALL);?>
 <link href='/build/css/style.css' type='text/css' rel='stylesheet'/>
 <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
-<script src="/build/js/three.min.js"></script>
-<script src="/build/js/lib/fileLoader.js"></script>
-<script src="/build/js/lib/controls/firstPerson.js"></script>
-<script src="/build/js/lib/controls/interaction.js"></script>
-<script src="/build/js/lib/controls/Player.js"></script>
-<script src="/build/js/lib/controls/PointerLockControls.js"></script>
-<script src="/build/js/lib/audio/audioLoader.js"></script>
-<script src="/build/js/lib/fire/VolumetricFire.js"></script>
-<script src="/build/js/lib/smoke/smoke.js"></script>
-<script src="/build/js/lib/fire/firePlacement.js"></script>
-<script src="/build/js/lib/stats/stats.min.js"></script>
-<script src="/build/js/lib/xml_loader/objects.js"></script>
-<script src="/build/js/game.js"></script>
+<script src="js/three.min.js"></script>
+<script src="js/lib/util/doSynchronousLoop.js"></script>
+<script src="js/lib/fileLoader.js"></script>
+<script src="js/lib/controls/firstPerson.js"></script>
+<script src="js/lib/controls/interaction.js"></script>
+<script src="js/lib/controls/Player.js"></script>
+<script src="js/lib/controls/PointerLockControls.js"></script>
+<script src="js/lib/audio/audioLoader.js"></script>
+<script src="js/lib/audio/interactionSounds.js"></script>
+<script src="js/lib/fire/VolumetricFire.js"></script>
+<script src="js/lib/smoke/smoke.js"></script>
+<script src="js/lib/fire/firePlacement.js"></script>
+<script src="js/lib/stats/stats.min.js"></script>
+<script src="js/lib/xml_loader/xml_loader.js"></script>
+<script src="js/game.js"></script>
