94b650d16d9a07dd4e341c6705a1e4ba482cedd5
dbarthol <dbarthol@cip54.informatik.uni-osnabrueck.de>
2016-08-01 11:21:53 +0200

    added new rooms

--- build/js/game.js
+++ build/js/game.js
@@ -71,7 +71,7 @@ var HEALTH_PER_SECOND = 10; if (godmode) {HEALTH_PER_SECOND = 0};// if fog is at
 
 function init(event) {
 
-	CreateSegment("groundlevel", 
+	CreateSegment("toilet", 
 	
     // set up the scene, the camera and the renderer
     function scene (){
