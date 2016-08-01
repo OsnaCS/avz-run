ae9346daa70c0b76a01d0375da5e633d3b16004e
mbeutel <mbeutel@cip58.informatik.uni-osnabrueck.de>
2016-08-01 14:44:15 +0200

    changed lvl

--- build/js/game.js
+++ build/js/game.js
@@ -70,7 +70,7 @@ var octreeObjects = [];
 function init(event) {
 
 
-	CreateSegment("lectureroom1",scene);
+	CreateSegment("groundlevel",scene);
 
     octree = new THREE.Octree( {
         // uncomment below to see the octree (may kill the fps)
